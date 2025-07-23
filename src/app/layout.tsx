c3ceada8333487bd82efd1110ee47a4477ee29cc
subhranshu <subhranshus@mindfiresolutions.com>
2025-07-23 22:31:00 +0530

    feat: fix the description in meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,13 +11,13 @@ const poppins = Poppins({
 
 export const metadata = {
   title: "Job Portal",
-  description: "Apply for jobs, post jobs, manage applications",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta name="description" content="Apply for jobs, post jobs, manage applications"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
           integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
           crossOrigin="anonymous" referrerPolicy="no-referrer" />
